e265209595786c150f4379ea9f6903dd6cc8b93a
Lucas Pereira Castanheira Nascimento <lucaspereira.cn@hotmail.com>
2022-03-23 19:23:35 -0300

    Cumpre requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './helper';
+import FavoritePokemons from '../components/FavoritePokemons';
+import pokemons from '../data';
+
+const pokemonsQuantity = 9;
+
+describe('teste do componente Favorite Pokémons', () => {
+  it('Testa se é exibido na tela a mensagem No favorite pokemon found', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ [] } />);
+    const message = screen.getByText('No favorite pokemon found');
+    expect(message).toBeInTheDocument();
+  });
+
+  it('Testa se é exibido todos os cards de pokémons favoritados', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ pokemons } />);
+    const favoritesPokemons = screen.getAllByTestId('pokemon-name');
+    expect(favoritesPokemons).toHaveLength(pokemonsQuantity);
+  });
+});
